06c5d2eaa2769512d1921180478a2944e666eec7
redikawest <redikawest@gmail.com>
2023-05-25 22:20:45 +0800

    add log

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # springboot-practice
+
+## Note
+```bash
+  @AllArgsConstructor lombok == constructor
+  @Data lombok == getter setter
+    
+```
--- src/main/java/com/example/practice/web/BookResources.java
+++ src/main/java/com/example/practice/web/BookResources.java
@@ -1,5 +1,8 @@
 package com.example.practice.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,16 +10,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.practice.dto.BookDetailDTO;
 import com.example.practice.service.logic.BookService;
 
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
+// @AllArgsConstructor
 @RestController
 public class BookResources {
+
+    Logger log = LoggerFactory.getLogger(BookResources.class);
     
     private final BookService bookService;
 
+    public BookResources(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/book/{bookId}")
     public BookDetailDTO findBookById(@PathVariable("bookId") Long id) {
-        return bookService.findBookDetailById(id);
+
+        StopWatch stopWatch = new StopWatch();
+        log.info("start find book detail "+ id);
+
+        stopWatch.start();
+
+        BookDetailDTO result = bookService.findBookDetailById(id);
+
+        log.info("finish fing book detail "+ stopWatch.getTaskCount());
+
+        return result;
     }
 }
+
+/**
+ * 
+ * Note.
+ * @AllArgsConstructor itu sama saja seperti constructor
+ * 
+ */
